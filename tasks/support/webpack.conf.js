d164e140fe74de0df16fefcb6eab7c96ed6236ea
Christian Maher <maher.cs@gmail.com>
2015-08-12 22:54:50 -0400

    Update for Karma 0.13

--- tasks/support/webpack.conf.js
+++ tasks/support/webpack.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
       preLoaders: [],
       loaders: [{
         test: /\.js$/,
-        exclude: 'node_modules',
+        exclude: /node_modules/,
         loader: 'babel'
       }]
     },
--- tasks/test.js
+++ tasks/test.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var karma = require('karma').server;
+var KarmaServer = require('karma').Server;
 var karmaConf = require('./support/karma.conf');
 
 module.exports = function (gulp, name, config) {
   gulp.task(name, 'Run tests in Chrome', function (done) {
-    karma.start(karmaConf(config), done);
+    var server = new KarmaServer(karmaConf(config), done);
+    server.start();
   }, {
     options: {
       dev: 'Leave browser open for debugging, watching for file changes'
