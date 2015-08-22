f502356fa6c8d2c4731f4e040151bb984add8060
Christian Maher <maher.cs@gmail.com>
2015-08-22 15:01:39 -0400

    Load sinon after sinon-chai to enable fakeServer

--- tasks/support/karma.conf.js
+++ tasks/support/karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai-as-promised', 'sinon-chai', 'chai', 'sinon'],
+    frameworks: ['mocha', 'chai-as-promised', 'sinon', 'sinon-chai', 'chai'],
 
     // list of files / patterns to load in the browser
     files: [
